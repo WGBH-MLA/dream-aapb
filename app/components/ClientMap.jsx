2e6c2e687af3b057569e61bcfecb511170cee23b
Harpo Harbert <ryan_harbert@wgbh.org>
2025-09-08 13:03:28 -0700

    Try: update dependencies

--- app/components/ClientMap.jsx
+++ app/components/ClientMap.jsx
@@ -1,21 +1,10 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
-export default function Mappy(props){
-  return (
-    <div className="mappy-container">
-      <MapContainer center={[37.8, -130]} zoom={2.5}>
-        <TileLayer attribution={`Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors`} url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapContainer>
-    </div>
-  )
-  
-import { useEffect } from "react"
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup, useMap, GeoJSON } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
 
-import usStates from "../data/usStates.json"
-import orgs from "../data/orgs.json"
+import usStates from '../data/usStates.json'
+import orgs from '../data/orgs.json'
 
-export default function Mappy(props){
+export default function Mappy(props) {
   // aggregate by state oh great
   let orgsByState = {}
   Object.keys(orgs).forEach(org_id => {
@@ -26,7 +15,7 @@ export default function Mappy(props){
       orgsByState[state] = {}
     }
     orgsByState[state][org_id] = org
-  })  
+  })
 
   const stateStyle = {
     fillColor: '#7c147c',
@@ -38,43 +27,58 @@ export default function Mappy(props){
 
   var sdata
   // var mapData = orgsByState.values.map( () )
-  var mapFeatures = usStates.features.map( (stateFeature) => {
+  var mapFeatures = usStates.features.map(stateFeature => {
     stateFeature.style = stateStyle
     return stateFeature
   })
   sdata = {
-   "type": "FeatureCollection",
-   "features": mapFeatures
+    type: 'FeatureCollection',
+    features: mapFeatures,
   }
 
   var markers = []
-  Object.values(orgsByState).map((orgsInState) => {
-    Object.keys(orgsInState).map((orgId) => {
+  Object.values(orgsByState).map(orgsInState => {
+    Object.keys(orgsInState).map(orgId => {
       let org = orgs[orgId]
-      let logo = org.Logo ? <img src={ `https://s3.amazonaws.com/americanarchive.org/org-logos/${org.Logo}` } className="mappy-logo" /> : null
+      let logo =
+        org.Logo ?
+          <img
+            src={`https://s3.amazonaws.com/americanarchive.org/org-logos/${org.Logo}`}
+            className='mappy-logo'
+          />
+        : null
       markers.push(
-        <Marker position={ {lat: org.location[0], lng: org.location[1] } } >
+        <Marker position={{ lat: org.location[0], lng: org.location[1] }}>
           <Popup>
-            <a href={ `/organizations/${orgId}` } target="_blank" class="org-url">
+            <a href={`/organizations/${orgId}`} target='_blank' class='org-url'>
               <h3>{org.Name}</h3>
             </a>
-            <div>{org.City}, {org.State}</div>
-            { logo }
-            <a href="/search?orgsid?????todo" target="_blank" class="btn btn-default btn-sm">View all records</a>
+            <div>
+              {org.City}, {org.State}
+            </div>
+            {logo}
+            <a
+              href='/search?orgsid?????todo'
+              target='_blank'
+              class='btn btn-default btn-sm'>
+              View all records
+            </a>
           </Popup>
         </Marker>
-      )  
+      )
     })
   })
-
 
   return (
     <>
       <MapContainer center={[37.8, -130]} zoom={2.5}>
-        <TileLayer attribution={`Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors`} url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer
+          attribution={`Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors`}
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
 
         <GeoJSON data={sdata} />
-        { markers }
+        {markers}
       </MapContainer>
     </>
   )
