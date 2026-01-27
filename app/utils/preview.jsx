1e0cf876d694871d24cb534452a63a571c4e50ac
fog <foglaboratories@gmail.com>
2026-01-27 15:49:15 -0500

    'preview url back to ov-frontend version'

--- app/utils/preview.jsx
+++ app/utils/preview.jsx
@@ -1,6 +1,6 @@
 export async function getPreview(id, content_type, token) {
   // exhibit_page or collection_page
-  let url = `${process.env.WAGTAIL_HOST}/preview/${id}/?content_type=${content_type}&token=${token}&format=json`
+  let url = `${process.env.WAGTAIL_HOST}/page_preview/${id}/?content_type=${content_type}&token=${token}`
   return await fetch(url, (res) => {
     console.log("preview", res)
   })
