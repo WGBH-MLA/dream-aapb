bbfd0ba2ba2ee274baf55c3b8bbe2a3726b1e811
fog <foglaboratories@gmail.com>
2026-03-03 12:50:14 -0500

    'build preview url with aapb api env var'

--- app/utils/preview.jsx
+++ app/utils/preview.jsx
@@ -1,7 +1,6 @@
 export async function getPreview(id, content_type, token) {
   // exhibit_page or collection_page
-  // let url = `${process.env.AAPB_API_URL}/page_preview/${id}/?content_type=${content_type}&token=${token}`
-  let url = `https://ov-wag-pr-258.dev.wgbh-mla.org/page_preview/${id}/?content_type=${content_type}&token=${token}`
+  let url = `${process.env.AAPB_API_URL}/page_preview/${id}/?content_type=${content_type}&token=${token}`
   return await fetch(url, (res) => {
     console.log("preview", res)
   })
