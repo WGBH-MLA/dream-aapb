db2985b73e9d13be0c5f32295bdfd145e5e1b127
fog <foglaboratories@gmail.com>
2026-04-07 10:54:31 -0400

    'vjs basic'

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -4,13 +4,34 @@ import thumbnailURL from "../utils/thumbnailURL"
 export default function VideoPlayer(props){
   if(!props.guid){ return null }
 
+  useEffect(() => {
+
+  })
+  var player = videojs('vjs-player', options, function onPlayerReady() {
+    videojs.log('Your player is ready!');
+
+    // In this context, `this` is the player that was created by Video.js.
+    this.play();
+
+    // How about an event listener?
+    this.on('ended', function() {
+      videojs.log('whoa mama!!');
+    });
+  });
+
+
   if(props.mediaURL){
     return (
+      // <div className="video-player-container">
+      //   <video poster={ thumbnailURL(props.guid) } controls preload="auto" width="640" height="480">
+      //     <source src={ props.mediaURL || "/A_Colour_Box_512kb.mp4" } />
+      //   </video>
+      // </div>
       <div className="video-player-container">
-        <video poster={ thumbnailURL(props.guid) } controls preload="auto" width="640" height="480">
+        <video id="vjs-player" poster={ thumbnailURL(props.guid) } controls preload="auto" width="640" height="480">
           <source src={ props.mediaURL || "/A_Colour_Box_512kb.mp4" } />
         </video>
-      </div>    
+      </div>
     )
   } else {
     return (
