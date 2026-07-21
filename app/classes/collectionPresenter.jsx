add94650788978ba4362c4057b1e19c8910537f7
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2026-07-21 14:42:14 -0400

    pretty collections, how beautiful

--- app/classes/collectionPresenter.jsx
+++ app/classes/collectionPresenter.jsx
@@ -30,10 +30,10 @@ export function renderCollection(collection, esConfig) {
 
   if (niceItems.length > 0) {
     niceItemsContainer = (
-
-      <div className="skinny-body-container">
-        <h2 className="smarbot">Featured Items</h2>
-        <div className="page-body marbot">
+    <div className="collection-right">
+      <div className="collection-section"></div>
+      <h2 className="smarbot">Featured Items</h2>
+      <div className="page-body marbot">
           {/* for baby <div className="items-search smarbot"></div>*/}
           <div className="nice-items-container marbot">
             <div className="nice-items-wrapper">
@@ -45,7 +45,12 @@ export function renderCollection(collection, esConfig) {
     )
   }
 
-    let collectionSearch = (<LayoutSearch
+  let viewCollectionURL = `/search?${esConfig.esIndex}[refinementList][special_collections][0]=${specialCollectionTag}`
+
+
+    let collectionSearch =
+      <div className="search-box-container">
+        <LayoutSearch
         navigateHook={ navigateHook }
         esIndex={ esConfig.esIndex }
         handleChange={ handleCollectionSearch }
@@ -53,63 +58,71 @@ export function renderCollection(collection, esConfig) {
         searchFilter={ `&${esConfig.esIndex }[refinementList][special_collections][0]=${ specialCollectionTag }`}
         wide={ true }
         placeholder={"Search the collection..."}
-      />)
+      />
 
-      let leftBlocks = renderBlocks(collection.content.filter(block =>
-        ["help", "resources"].includes(block.type)
-      ))
-
-      let rightBlocks = renderBlocks(collection.content.filter(block =>
-        ["background"].includes(block.type)
-      ))
-
-    return (
-    <div>
-      <div className="page-container">
-        <div className="collection-header">
-          <h2>{ dangerousDiv(collection.title, false) }</h2>
-          <a className="top-back-link" href="/collections">&lt; Back To Special Collections</a>
-        </div>
-        <div className="collection-main-grid">
-
-          <div className="collection-left">
-            <div className="collection-image-container">
-              <img src={ collection.cover_medium.full_url } />
-            </div>
-
-            <div className="skinny-body-container collection-search marbot">
-              { collectionSearch }
-            </div>
-
-            <div className="skinny-body-container">
-              <div className="page-body">
-                { leftBlocks }
-              </div>
-            </div>
+      <div className="search-meta">
+        <a className="view-all-shadow" href={ viewCollectionURL }>
+          View the collection
+          </a>
           </div>
-
-          <div className="collection-right">
-            <div className="skinny-body-container">
-              <h2 className="smarbot">Collection Summary</h2>
-              <div className="page-body">
-                { dangerousDiv(collection.introduction) }
-              </div>
-              <hr />
-            </div>
-
-            <div className="skinny-body-container">
-              <div className="page-body">
-                { rightBlocks }
-              </div>
-              <hr />
-            </div>
-
-            { niceItemsContainer }
-          </div>
-
         </div>
 
-      </div>
+        let leftBlocks = (
+  <div className="collection-resources">
+    { renderBlocks(collection.content.filter(block =>
+      ["resources"].includes(block.type)
+    )) }
+  </div>
+)
+  let rightBlocks = renderBlocks(collection.content.filter(block =>
+    ["background"].includes(block.type)
+  ))
+
+return (
+  <div className="page-container collection-page-container">
+    <div className="collection-header">
+      <h2>{ dangerousDiv(collection.title, false) }</h2>
     </div>
-  )
+    <a className="top-back-link" href="/collections">&lt; Back To Special Collections</a>
+
+    <div className="collection-main-grid">
+
+      <div className="collection-left">
+        <div className="collection-image-container">
+          <img src={ collection.cover_medium.full_url } />
+        </div>
+
+        <div className="collection-section collection-search marbot">
+          { collectionSearch }
+        </div>
+
+        <div className="collection-section">
+          <div className="page-body">
+            { leftBlocks }
+          </div>
+        </div>
+      </div>
+
+      <div className="collection-right">
+        <div className="collection-section">
+          <h2 className="smarbot">Collection Summary</h2>
+          <div className="page-body">
+            { dangerousDiv(collection.introduction) }
+          </div>
+          <hr />
+        </div>
+
+        <div className="collection-section">
+          <div className="page-body">
+            { rightBlocks }
+          </div>
+          <hr />
+        </div>
+
+        { niceItemsContainer }
+      </div>
+
+    </div>
+  </div>
+)
 }
